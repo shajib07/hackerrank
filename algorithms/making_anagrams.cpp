e31c886fc79b932ae10c0e93a4d1498357e32694
Md Atahar Hossain <shajib.ruet07@gmail.com>
2019-03-05 07:35:15 +0100

    Create making_anagrams.cpp

---
+++ algorithms/making_anagrams.cpp
@@ -0,0 +1,29 @@
+#include <cstring>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define MAX 26
+
+int main() {
+	
+  int ans = 0;
+	int count[MAX] = {0};
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	for(unsigned int i = 0; i < s1.length(); i++) {
+		count[s1[i] - 'a']++;
+	}
+
+	for(unsigned i = 0; i < s2.length(); i++) {
+		count[s2[i] - 'a']--;
+	}
+
+	for(int i = 0; i < MAX; i++) {
+		ans += abs(count[i]);
+	}
+  
+	cout << ans << endl;
+	return 0;
+}
