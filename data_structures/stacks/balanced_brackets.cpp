b9e98464b26e718f4e346e25e2121a96b5b798fc
Md Atahar Hossain <shajib.ruet07@gmail.com>
2019-03-09 16:37:03 +0100

    Create balanced_brackets.cpp

---
+++ data_structures/stacks/balanced_brackets.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <stack>
+using namespace std;
+
+void isBalanced(string s) {
+
+	stack<char> st;
+
+	for (int i = 0; i < s.length(); i++) {
+
+		switch(s[i]) {
+
+			case '[':
+			case '{':
+			case '(':
+//				cout << "push to stack " << s[i] <<endl;
+				st.push(s[i]);
+				break;
+			case ']':
+				if (st.empty() || st.top() != '[') {
+					cout << "NO" << endl;
+					return;
+				}
+				st.pop();
+				break;
+
+			case '}':
+				if (st.empty() || st.top() != '{') {
+					cout << "NO" << endl;
+					return;
+				}
+				st.pop();
+				break;
+
+			case ')':
+				if (st.empty() || st.top() != '(') {
+					cout << "NO" << endl;
+					return;
+				}
+				st.pop();
+				break;
+		}
+	}
+
+	if(st.empty()) {
+		cout << "YES"<<endl;
+	} else {
+		cout << "NO"<<endl;
+	}
+
+}
+int main() {
+
+	int n;
+	cin >> n;
+	string str[n];
+	for (int l = 0; l < n; l++) {
+		cin >> str[l];
+	}
+
+	for (int l = 0; l < n; l++) {
+		isBalanced(str[l]);
+
+	}
+	return 0;
+}
